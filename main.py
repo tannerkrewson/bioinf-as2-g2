53c77df37addf35988fc3d0ce5afa5f51c64b783
tannerkrewson <tannerkrewson@gmail.com>
2018-11-08 22:27:07 -0600

    Wrote general structure of program

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from readfasta import readfasta
-from random import randint
-from scipy import stats
 import glob, os
 
 '''
@@ -13,10 +11,51 @@ def main():
     # scan in all fasta files in the "genes" directory
     os.chdir( os.getcwd() + "/genes/" )
     for file in glob.glob( "*.fasta" ):
-        print( file )
 
+        # read all the genes from the fasta file
+        print( file )
         genes = readfasta( file )
-        for gene in genes:
-            print( gene[1][:50] )
+
+        original_tree = generate_tree(genes)
+
+        # count the clades of the original tree and add them as keys
+        clade_count_dict = build_clade_count_dict( original_tree )
+
+        BOOTSTRAP_TIMES = 10
+
+        # count each clade in bootstrapped trees that match a clade from the original tree
+        for i in range (0, BOOTSTRAP_TIMES):
+            bootstrapped_genes = get_bootstrapped_genes( genes )
+
+            this_tree = generate_tree( genes )
+
+            count_clades( this_tree, clade_count_dict )
+
+        # find the confidence of each clade
+        for clade in clade_count_dict:
+            the_count = clade_count_dict[clade]
+
+            clade_count_dict[clade] = the_count / BOOTSTRAP_TIMES
+
+        # at this point, the clade_count_dict now maps clades to their respective confidence level
+        print( clade_count_dict )
+
+
+def generate_tree(genes):
+    # find the distance between each gene
+    distance_matrix = []
+    for i in range(0, len(genes)):
+
+        # make it 2d by adding new list everytime
+        new_row = []
+
+        for j in range(i, len(genes)):
+            new_row.append( find_distance(genes[i], genes[j]) )
+        
+        distance_matrix.append( new_row )
+
+    # use upgma to generate a tree from the distance matrix
+    return calculate_upgma( distance_matrix )
+
 
 main()
