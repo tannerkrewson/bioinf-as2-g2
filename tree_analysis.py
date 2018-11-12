d1d49ef1008b6afef3d6eb404352fe33228989db
Domenic Cusanelli <stl3339@gmail.com>
2018-11-11 18:16:24 -0600

    added clade_search function to count clades of a tree into a dictionary

--- tree_analysis.py
+++ tree_analysis.py
@@ -1,5 +1,13 @@
 def build_clade_count_dict( original_tree ):
-    return False
+    return clade_count_dict
 
-def count_clades( tree, clade_count_dict ):
-    return False
+def clade_search( boots_tree, clade_count_dict )
+    if type(boots_tree[0] == tuple):
+        clade_search(boots_tree[0], clade_count_dict)
+    if type(boots_tree[1] == tuple):
+        clade_search(boots_tree[1], clade_count_dict)
+
+    if boots_tree in clade_count_dict:
+        clade_count_dict[boots_tree] += 1
+    
+    return clade_count_dict
