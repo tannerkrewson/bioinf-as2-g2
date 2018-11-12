f7a45c538407289eb985150aca9358d3a6e2c652
Domenic Cusanelli <stl3339@gmail.com>
2018-11-11 19:03:46 -0600

    filled out function to generate BS genes and calculate clade confidences

--- bootstrapping.py
+++ bootstrapping.py
@@ -1,2 +1,25 @@
-def get_bootstrapped_genes( gene_list ):
+def generate_bootstrap_genes( sequences ):
+    new_sequences = []
+
+    align_all_sequences( sequences )
+
+    sequence_length = 1 # TODO: add correct value
+
+    for i in range(0, sequence_length):
+        rand_pos = random.randint(0, sequence_length)
+        for j in range(0, len(sequences)):
+            new_sequences[j] += sequences[j][randpos]
+
+    return new_sequences
+
+def align_all_sequences( sequences ):
+    # TODO: align all sequences so they can be picked from for bootstrapping
     return False
+
+def calculate_confidences( clade_count_dict, bootstrap_num ):
+    confidences = {}
+
+    for i, j in clade_count_dict.items():
+        confidences[i] = j / bootstrap_num
+
+    return confidences
