3384ed4ea19e3c4eb194dca493eaf3756db6d052
Domenic Cusanelli <stl3339@gmail.com>
2018-11-11 18:25:52 -0600

    added build_clade_count_dict function

--- tree_analysis.py
+++ tree_analysis.py
@@ -1,5 +1,10 @@
-def build_clade_count_dict( original_tree ):
-    return clade_count_dict
+def build_clade_count_dict( original_tree, clade_count_dict ):
+    if type(original_tree[0] == tuple):
+        build_clade_count_dict(original_tree, clade_count_dict)
+    if type(original_tree[1] == tuple):
+        build_clade_count_dict(original_tree, clade_count_dict)
+
+    clade_count_dict[original_tree] = 0
 
 def clade_search( boots_tree, clade_count_dict )
     if type(boots_tree[0] == tuple):
@@ -9,5 +14,3 @@ def clade_search( boots_tree, clade_count_dict )
 
     if boots_tree in clade_count_dict:
         clade_count_dict[boots_tree] += 1
-    
-    return clade_count_dict
