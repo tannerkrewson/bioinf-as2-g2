8b32b8e5095ca5c6c133e8985b534871c50a80ed
Domenic Cusanelli <stl3339@gmail.com>
2018-11-11 18:34:25 -0600

    added empty functions to upgma class

--- upgma.py
+++ upgma.py
@@ -1,2 +1,12 @@
 def calculate_upgma( distance_matrix ):
     return False
+
+def refactor_matrix( distance_matrix ):
+    # for fixing the matrix after creating a clade
+    return False
+
+def find_lowest_distance( distance_matrix ):
+    return False
+
+def generate_matrix( sequences ):
+    return False
